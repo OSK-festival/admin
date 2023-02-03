2fb1735cc3c2ca66287a8284df2e78378c4f0cfe
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-02-03 22:47:35 +0900

    update: Vuetifyでサイトをデザインした．

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  srcDir: 'src/',
+  runtimeConfig: {
+    SUPABASA_URL: process.env.SUPABASE_URL,
+    SUPABASA_KEY: process.env.SUPABASE_KEY
+  },
+  modules: [
+    "@nuxtjs/supabase"
+  ],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "mdi/css/materialdesignicons.min.css",
+    '/assets/style/style.css',
+  ],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@400;500;700&display=swap'
+        }
+      ]
+    }
+  }
 })
---
+++ src/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar title="記念祭運営システム">
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" location="right" temporary>
+      メニュー
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-main>
+        <v-container>
+          <NuxtPage />
+        </v-container>
+      </v-main>
+    </v-main>
+  </v-app>
+
+</template>
+
+<script setup>
+const links = [
+  'Dashboard',
+  'Messages',
+  'Profile',
+  'Updates',
+]
+
+const drawer = ref(false);
+
+</script>
