200b9208f2dfbaafa17288c1619c5fe4301e9861
yas-ako <105139975+yas-ako@users.noreply.github.com>
2023-02-12 20:43:23 +0900

    update: baseURL admin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    // baseURL: "/admin/",
+    baseURL: "/admin/",
     // cdnURL: "https://osk-festival.github.io/admin/",
     head: {
       meta: [
